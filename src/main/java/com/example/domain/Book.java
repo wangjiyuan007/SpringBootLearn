e6184c66e1b8e51124c97c956aeb4c98f4e06d52
wangjiyuan007 <wangjiyuan@whu.edu.cn>
2019-07-02 13:55:51 +0800

    添加redis连接实例

---
+++ src/main/java/com/example/domain/Book.java
@@ -0,0 +1,11 @@
+package com.example.domain;
+
+import lombok.Data;
+
+import java.io.Serializable;
+@Data
+public class Book implements Serializable {
+    private Integer id;
+    private String name;
+    private String author;
+}
